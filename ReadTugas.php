fd4f89228996d049b523d868ae71a723c7aa04e2
Kinaya0409 <230712402@students.uajy.ac.id>
2025-11-28 16:41:11 +0700

    .

---
+++ ReadTugas.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Jadwal Belajar</title>
+</head>
+<body>
+<h2>CRUD Jadwal Belajar</h2>
+<p><a href="Read.php">Beranda</a> / <a href="Create.php">Tambah Jadwal</a></p>
+
+<h3>Data Jadwal</h3>
+
+<table cellpadding="5" cellspacing="0" border="1">
+    <tr bgcolor="#CCCCCC">
+        <th>No.</th>
+        <th>Mata Kuliah</th>
+        <th>Hari</th>
+        <th>Jam</th>
+        <th>Opsi</th>
+    </tr>
+
+    <?php
+    // include file koneksi ke database
+    include("../koneksi.php");
+
+    // ambil data jadwal dari database
+    $query = mysqli_query($koneksi, "SELECT * FROM jadwal ORDER BY id DESC") or die(mysqli_error($koneksi));
+
+    // cek apakah data ada di database
+    if(mysqli_num_rows($query) == 0){
+        echo '<tr><td colspan="5">Tidak ada data!</td></tr>';
+    } else {
+        $no = 1;
+        while($data = mysqli_fetch_assoc($query)){
+            echo '<tr>';
+            echo '<td>'.$no.'</td>';
+            echo '<td>'.$data['matkul'].'</td>';
+            echo '<td>'.$data['hari'].'</td>';
+            echo '<td>'.$data['jam_mulai'].' - '.$data['jam_selesai'].'</td>';
+            echo '<td><a href="Edit.php?id='.$data['id'].'">Edit</a> / 
+                       <a href="Delete.php?id='.$data['id'].'" onclick="return confirm(\'Yakin?\')">Hapus</a></td>';
+            echo '</tr>';
+            $no++;
+        }
+    }
+    ?>
+
+</table>
+
+</body>
+</html>
